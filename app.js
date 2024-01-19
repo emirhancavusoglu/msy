3425ffcc17ce003181401ea0ce84f0512f81f38d
emirhancavusoglu <emirhan.cavusoglu.20513@ozu.edu.tr>
2024-01-20 02:12:55 +0300

    ss

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
